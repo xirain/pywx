d18e89a8014bf4ffda906d80ee8978321f43700c
xirain <wangy675@163.com>
2015-09-16 20:11:57 +0800

    youdao api

--- flask_app.py
+++ flask_app.py
@@ -7,6 +7,8 @@ sys.setdefaultencoding('utf-8')
 from flask import Flask, request, make_response
 import hashlib
 import xml.etree.ElementTree as ET
+import urllib2, json, urllib
+import time
 
 app = Flask(__name__)
 
@@ -28,6 +30,37 @@ def weixin_verify():
             return echostr #success
         return 'access verification fail' #fail
 
+def getFanyi(word):
+    word = urllib2.quote(word)
+    # apiuri = 'http://fanyi.youdao.com/openapi.do?keyfrom=flaskr&key=1272049953&type=data&doctype=json&version=1.1&q={}'.format(word)
+    handler=urllib2.HTTPHandler(debuglevel=1)
+    opener = urllib2.build_opener(handler)
+    urllib2.install_opener(opener)
+    url = 'http://fanyi.youdao.com/openapi.do'
+    values = {'keyfrom':'flaskr',
+              'key':1272049953,
+              'type':'data',
+              'doctype':'json',
+              'version':1.1,
+              'q':word}
+    data = urllib.urlencode(values)
+    req = urllib2.Request(url, data)
+    try:
+        resp = urllib2.urlopen(req)
+        jsonData = json.loads(resp.read())
+
+        # print json.dumps(jsonData,indent=2)
+        errorCode = jsonData['errorCode']
+        if 0 == errorCode:
+            trans = u'\n{}的翻译：{}.\n 网络解释：{}\n'.format(jsonData['query'], ';'.join(jsonData['basic']['explains']), ';'.join(jsonData['web'][0]['value']))
+        else:
+            trans = u'翻译出错，错误码 {}'.format(errorCode)
+    except Exception, e:
+        trans = str(e)
+    
+    return trans
+
+
 @app.route('/weixin', methods=['POST'])
 def weixin_echo_you_said():
     str_xml = request.data
@@ -38,6 +71,9 @@ def weixin_echo_you_said():
     to_user = xml.find("ToUserName").text
     # msg_id = xml.find("MsgId").text
 
+    if type(content).__name__ == "unicode":
+            content = content.encode('UTF-8')
+    content = getFanyi(content)
     response_data = """
 <xml>
 <ToUserName><![CDATA[{}]]></ToUserName>
@@ -51,7 +87,7 @@ def weixin_echo_you_said():
     to_user,
     str(int(time.time())),
     msg_type,
-    u"我现在还在开发中，还没有什么功能，您刚才说的是：" + content)
+    content)
     response = make_response(response_data)
     response.content_type = 'application/xml'
     return response
